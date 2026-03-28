9355a0497e27b2a9b0aa72604a53ef0108332867
duquangtrung <duquangtrung2004@gmail.com>
2026-03-28 22:40:13 +0700

    update

--- bth5/app/src/main/java/com/trung/bth5/MainActivity.java
+++ bth5/app/src/main/java/com/trung/bth5/MainActivity.java
@@ -2,6 +2,7 @@ package com.trung.bth5;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -11,23 +12,38 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
+
+    }
+    void TimDieuKhien(){
+        editTextSo1 = (EditText) findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText) findViewById(R.id.edtSo2);
+        editTextKQ= (EditText) findViewById(R.id.edtKetQua);
+        nutCong=(Button) findViewById(R.id.btnCong);
+        nutCong=(Button) findViewById(R.id.btnTru);
+        nutCong=(Button) findViewById(R.id.btnNhan);
+        nutCong=(Button) findViewById(R.id.btnChia);
+
 
     }
     void xulicong(View v) {
-        EditText editTextSo1= (EditText) findViewById(R.id.edtSo1);
-        EditText editTextSo2= (EditText) findViewById(R.id.edtSo2);
+
         String soThu1 = editTextSo1.getText().toString();
         String soThu2 = editTextSo2.getText().toString();
         float soA = Float.parseFloat(soThu1);
         float soB = Float.parseFloat(soThu2);
         float Tong = soA + soB;
-        EditText editTextKQ= (EditText) findViewById(R.id.edtKetQua);
+
         String chuoiKQ = String.valueOf(Tong);
         editTextKQ.setText(chuoiKQ);
 
@@ -36,8 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     void xulitru( View v) {
-        EditText editTextSo1= (EditText) findViewById(R.id.edtSo1);
-        EditText editTextSo2= (EditText) findViewById(R.id.edtSo2);
+
         String soThu1 = editTextSo1.getText().toString();
         String soThu2 = editTextSo2.getText().toString();
         float soA = Float.parseFloat(soThu1);
@@ -45,13 +60,12 @@ public class MainActivity extends AppCompatActivity {
         float Tong = soA - soB;
         EditText editTextKQ= (EditText) findViewById(R.id.edtKetQua);
         String chuoiKQ = String.valueOf(Tong);
-        editTextKQ.setText(chuoiKQ);
+
 
 
     }
     void xulinhan( View v){
-        EditText editTextSo1= (EditText) findViewById(R.id.edtSo1);
-        EditText editTextSo2= (EditText) findViewById(R.id.edtSo2);
+
         String soThu1 = editTextSo1.getText().toString();
         String soThu2 = editTextSo2.getText().toString();
         float soA = Float.parseFloat(soThu1);
@@ -59,13 +73,12 @@ public class MainActivity extends AppCompatActivity {
         float Tong = soA * soB;
         EditText editTextKQ= (EditText) findViewById(R.id.edtKetQua);
         String chuoiKQ = String.valueOf(Tong);
-        editTextKQ.setText(chuoiKQ);
+
 
 
     }
     void xulichia(View v) {
-        EditText editTextSo1= (EditText) findViewById(R.id.edtSo1);
-        EditText editTextSo2= (EditText) findViewById(R.id.edtSo2);
+
         String soThu1 = editTextSo1.getText().toString();
         String soThu2 = editTextSo2.getText().toString();
         float soA = Float.parseFloat(soThu1);
@@ -73,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         float Tong = soA / soB;
         EditText editTextKQ= (EditText) findViewById(R.id.edtKetQua);
         String chuoiKQ = String.valueOf(Tong);
-        editTextKQ.setText(chuoiKQ);
+
 
 
     }
